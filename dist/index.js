ebdccdea8b26db3a8ea9db2ef030719f8970e1ed
samualhalder <samual.halder2020@gmail.com>
2024-05-11 12:14:12 +0530

    2:how to declare variable and funtions

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
 "use strict";
+//way to declare a variable
 var a = "samual";
-console.log(a);
+a = "true";
+var b;
+b = "hello";
+b = 234;
+let c = "hello world";
+//how to declare a function
+const fun = (m, n) => {
+    // console.log(m,"=",n);
+};
+// console.log(b);
+// console.log(a);
+fun("value", 23);
+// how to set the return type of a funtion
+const fun2 = (n, m) => {
+    console.log("its only returns a number");
+    return m * 10;
+};
+let x = fun2("value", 14);
+console.log(x);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
+//way to declare a variable
 var a:string="samual"
-console.log(a);
+a="true"
+var b:string|number;
+b="hello"
+b=234;
+let c=<string>"hello world"
+
+//how to declare a function
+const fun=(m:string,n:number)=>{
+    // console.log(m,"=",n);
+    
+}
+// console.log(b);
+
+// console.log(a);
+fun("value",23)
+
+// how to set the return type of a funtion
+const fun2=(n:string,m:number):number =>{
+     console.log("its only returns a number");
+     
+    return m*10;
+}
+
+let x:number=fun2("value",14)
+console.log(x);
+
