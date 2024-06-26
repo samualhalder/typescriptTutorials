355236b0e86f86bc904c8325d6eede120d076b01
samualhalder <samual.halder2020@gmail.com>
2024-06-26 13:14:27 +0530

    useState & useEffect

--- react-typesctipt/src/App.tsx
+++ react-typesctipt/src/App.tsx
@@ -1,12 +1,32 @@
-import Card from "./components/Card"
+import { useEffect, useState } from "react"
+
+type User={
+  name:string,
+  age:number
+}
 
 
 function App() {
-   const name:string="Samual";
+
+   const [user, setuser] = useState<User>()
+   const handleSubmit=(e:React.FormEvent<HTMLFormElement>):void=>{
+    e.preventDefault()
+    console.log("sublitted");
+    
+    console.log(user);
+   }
+ 
+   const handleChange=(e:React.ChangeEvent<HTMLInputElement>):void=>{
+      setuser((pre)=>({...pre!,[e.target.name]: e.target.name==='name'?e.target.value:+e.target.value}))
+   }
+
   return (
     <>
-      <h1>Hello world</h1>
-      <Card name={name} age={23} fun={()=>alert('hey')}/>
+      <form  onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" value={user?.name || ""} onChange={e=>handleChange(e)}/>
+        <input type="text" name="age" id="" value={user?.age || 0} onChange={(e)=>handleChange(e)} />
+         <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
--- react-typesctipt/src/components/Card.tsx
+++ react-typesctipt/src/components/Card.tsx
@@ -1,17 +1,28 @@
+import React, { useState } from "react"
 
+type Props={
+  name:string,
+  age:number,
+ setter:(React.Dispatch<React.SetStateAction<string>>),
+}
 
-function Card({name,age,fun}:{
-    name:string,
-    age:number,
-    fun:()=>void,
-}) {
-    fun();
+type User={
+  name:string,
+  age:number
+}
+
+function Card({name,age,setter}:Props) {
+    const [user, setuser] = useState<User>()
   return (
     <div>
         <h3>
+
             its iniside a Card {name},age {age}
         </h3>
-        
+        <input type="text" placeholder="enter somethinng" name="" id="" onChange={(e)=>setter(e.target.value)}/>
+        <form action="">
+
+        </form>
     </div>
   )
 }
